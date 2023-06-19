fec101cb261884a53219fa8c3bdf9a4ccc5510dd
Anastasia-Yukhno <nasta5.666666@gmail.com>
2023-06-19 10:12:49 +0200

    fix: delete unused code

--- server/index.js
+++ server/index.js
@@ -8,12 +8,6 @@ const cors = require("cors");
 
 app.use(cors());
 
-app.get("/api", (req, res) => {
-  res.json({
-    message: "Hello world",
-  });
-});
-
 http.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
 });
